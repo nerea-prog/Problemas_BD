a714ce7c0d25b88cc6f02a8e059e7df1fc0649c2
nerea-prog <narvaeznerea18@gmail.com>
2025-03-05 12:35:03 +0100

    Lorena callate

--- fase4_LorenayNerea.sql
+++ fase4_LorenayNerea.sql
@@ -10,17 +10,27 @@
 SELECT a.nom AS ciutat, a.ciutat AS aeroport, v.data, v.codi
 FROM aeroport a, vol v
 WHERE a.codi = v.aeroport_origen
-	AND durada < 40
-	AND DATA BETWEEN '2024/02/01' AND '2024/02/29'
-	AND descripcio LIKE '%DELAYED%'
-	ORDER BY ciutat, DATA;
+AND durada < 40
+AND DATA BETWEEN '2024/02/01' AND '2024/02/29'
+AND descripcio LIKE '%DELAYED%'
+ORDER BY ciutat, DATA;
 
 --Pregunta 2
+SELECT a.any_fabricacio AS any, c.nom AS compania, a.num_serie, a.tipus
+FROM  avio a, companyia c
+WHERE a.companyia = c.nom
+AND pais LIKE '%spain%'
+AND any_fabricacio < 2000
+ORDER BY any_fabricacio DESC, compania ASC, num_serie ASC;
 
 --Pregunta 3
 
---Pregunta 4
 
+--Pregunta 4
+ SELECT CONCAT(p.cognom, ', ', p.nom) AS passatger
+ CONCAT(h.cognom, ', ', h.nom) AS hostessa
+ v.aeroport_origen, v.aeroport_desti, durada
+ FROM passatger p JOIN volar v ON()  
 --Pregunta 5
 
 --Pregunta 6
